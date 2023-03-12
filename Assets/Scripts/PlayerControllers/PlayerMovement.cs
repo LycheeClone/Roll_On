994a898f75527743a13156b749b996e6c0dca26b
LycheeClone <code.halildemir@gmail.com>
2023-03-12 11:31:45 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerControllers/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlayerControllers
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        private Rigidbody _playerRb;
+        [SerializeField] private float movementSpeed;
+
+        private void Awake()
+        {
+            _playerRb = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            ObjectMovement();
+        }
+        
+
+        private void ObjectMovement()
+        {
+            float xAxis = Input.GetAxis("Horizontal");
+            float zAxis = Input.GetAxis("Vertical");
+            float elapsedTime = Time.fixedDeltaTime;
+            float smoothMovement = movementSpeed * elapsedTime;
+            Vector3 move = new Vector3(xAxis * smoothMovement, 0, zAxis * smoothMovement);
+            _playerRb.AddForce(move,ForceMode.VelocityChange);
+        }
+    }
+}
